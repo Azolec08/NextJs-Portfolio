3032f3310ef7fe30d189347de49a1ce4dbece22d
Mark Celoza <markangeloceloza380@gmail.com>
2024-09-20 01:54:31 +0800

    adjust height

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="h-[2500px] md:h-[1400px] lg:h-[1000px]  container"
+      className="min-h-[2600px] md:min-h-[1500px] lg:min-h-[1200px]  container"
     >
       <div className="h-full w-full flex flex-col items-center justify-center">
         <TextGenerateEffect
